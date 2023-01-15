049d58b99ad4468a5865bcf9ae95ba0c24426aab
Kushagra Upadhyaya <kushagra23dec@gmail.com>
2023-01-15 17:53:12 +0530

    cc

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "../../style.css";
 
 const Nav = () =>{
+    const  [login,setLogin] = useState(false);
     return(
         <div className="nav-div">
             <Link className="rm-link-color" to="/"><h1> Food Heaven</h1></Link>
@@ -10,6 +11,7 @@ const Nav = () =>{
                 <li><Link className="rm-link-color" to="/about">About-Us</Link></li>
                 <li><Link className="rm-link-color" to="/contact">Contact-Us</Link></li>
                 <li> <Link className="rm-link-color" to="/cart">Cart</Link>   </li>
+                <li> <button className="log-in-out" onClick={e=>(login===false?setLogin(true):setLogin(false))} >{login===false?"Login":"Logout"}</button></li>
             </ul>
         </div>
     );
